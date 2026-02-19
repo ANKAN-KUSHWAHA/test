27f0dc67b1059eb4a262c89e7d55dcc1ca25f9b5
Ankan Kushwaha <ankankushwaha30@gmail.com>
2026-02-19 16:14:54 +0530

    first

---
+++ src/utils.py
@@ -0,0 +1,5 @@
+# src/utils.py
+import os   # unused import
+
+def add(a, b):
+    return a + b
---
+++ test.py
@@ -0,0 +1,5 @@
+# tests/test_utils.py
+from src.utils import add
+
+def test_add():
+    assert add(2, 3) == 5
